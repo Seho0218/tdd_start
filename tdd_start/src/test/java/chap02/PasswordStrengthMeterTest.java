5c2dafdc44c14cd62f38fdaa021c7d7f18303654
Seho0218 <ghdtpgh8913@gmail.com>
2023-03-24 19:04:06 +0900

    세 번째 테스트: 숫자를 포함하지 않고 나머지 조건은 충족하는 경우

--- tdd_start/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ tdd_start/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,9 +1,10 @@
 package test.java.chap02;
 
 import main.chap02.PasswordStrength;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordStrengthMeterTest {
 
@@ -12,18 +13,28 @@ public class PasswordStrengthMeterTest {
     void meetsAllCriteria_Then_Strong() {
         PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12@AB");
-        Assertions.assertEquals(PasswordStrength.STRONG, result);
+        assertEquals(PasswordStrength.STRONG, result);
         PasswordStrength result2 = meter.meter("abc!Add");
-        Assertions.assertEquals(PasswordStrength.STRONG, result2);
+        assertEquals(PasswordStrength.STRONG, result2);
     }
 
     @Test
-    @DisplayName("첫 번째 테스트 : 모든 규칙을 충족하는 경우")
+    @DisplayName("두 번째 테스트 : 길이만 8글자 미만이고 나머지 조건은 충족하는 경우")
     void meetsOtherCriteria_except_for_Length_Then_Normal() {
         PasswordStrengthMeter meter = new PasswordStrengthMeter();
         PasswordStrength result = meter.meter("ab12@A");
-        Assertions.assertEquals(PasswordStrength.NORMAL, result);
+        assertEquals(PasswordStrength.NORMAL, result);
         PasswordStrength result2 = meter.meter("Ab12!c");
-        Assertions.assertEquals(PasswordStrength.NORMAL, result2);
+        assertEquals(PasswordStrength.NORMAL, result2);
     }
+
+    @Test
+    @DisplayName("세 번째 테스트 : 숫자를 포함하지 않고 나머지 조건은 충족하는 경우")
+    void meetsOtherCriteria_except_for_number_Then_Normal(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab!@ABqwer");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
+
+
 }

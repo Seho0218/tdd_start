37da3cc08026dbfc4a13060fa7758e97a798dacf
Seho0218 <ghdtpgh8913@gmail.com>
2023-03-27 02:19:32 +0900

    p79. 예외 상황 테스트 진행 계속

--- tdd_start/src/test/java/chap03/ExpiryDateCalculator.java
+++ tdd_start/src/test/java/chap03/ExpiryDateCalculator.java
@@ -14,6 +14,19 @@ public class ExpiryDateCalculator {
     */
 
     public LocalDate calculateExpiryDate(PayData payData) {
+        if (payData.getFirstBillingDate() != null) {
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            if(payData.getFirstBillingDate().getDayOfMonth() !=
+            candidateExp.getDayOfMonth()){
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+
+           /*
+            if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
+                return LocalDate.of(2019, 3, 31);
+            }
+            */
+        }
         return payData.getBillingDate().plusMonths(1);
     }
 

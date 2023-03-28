3e7ff831f0b132a9c9198294ee344a0bf7c548bd
Seho0218 <ghdtpgh8913@gmail.com>
2023-03-28 22:31:46 +0900

    p89. 코드 정리

--- tdd_start/src/test/java/chap03/ExpiryDateCalculator.java
+++ tdd_start/src/test/java/chap03/ExpiryDateCalculator.java
@@ -17,26 +17,29 @@ public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if(payData.getFirstBillingDate().getDayOfMonth() !=
-            candidateExp.getDayOfMonth()){
-                if(YearMonth.from(candidateExp).lengthOfMonth() <
-                payData.getFirstBillingDate().getDayOfMonth()){
-                    return candidateExp.withDayOfMonth(
-                            YearMonth.from(candidateExp).lengthOfMonth()
-                    );
+            LocalDate candidateExp =
+                    payData.getBillingDate().plusMonths(addedMonths);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+                final int dayLenOfCandiMon =
+                        YearMonth.from(candidateExp).lengthOfMonth();
+                if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
                 }
-
-                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+                return candidateExp.withDayOfMonth(dayOfFirstBilling);
+            } else {
+                return candidateExp;
             }
+        } else {
 
            /*
             if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
                 return LocalDate.of(2019, 3, 31);
             }
             */
-        }
-        return payData.getBillingDate().plusMonths(addedMonths);
-    }
 
+            return payData.getBillingDate().plusMonths(addedMonths);
+        }
+    }
 }
+
